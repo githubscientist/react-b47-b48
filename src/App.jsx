78d5b7aa867887529355dc9ddf2ff8285d7fca72
githubscientist <sathish.datascientist@gmail.com>
2023-09-17 18:28:25 +0530

    dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@
 // for now it does not receive any arguments
 const App = () => {
   const now = new Date();
+  const a = 10;
+  const b = 20;
+
+  console.log(now, a + b);
 
   // returns a html
   return (
     <div>
-      <p>Hello World!, it is { now.toString() }</p>
+      <p>Hello World!, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is { a + b } 
+      </p> <br />
     </div>
   )
 }
