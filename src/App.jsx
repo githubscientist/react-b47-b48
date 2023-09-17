98e184e92c29f762f12998233846ceacefd5341c
githubscientist <sathish.datascientist@gmail.com>
2023-09-17 18:32:00 +0530

    multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-// our first react component
-// javascript function 
-// for now it does not receive any arguments
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-
-  console.log(now, a + b);
-
-  // returns a html
+const Hello = () => {
   return (
     <div>
-      <p>Hello World!, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is { a + b } 
-      </p> <br />
+      <p>Hello World!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
     </div>
   )
 }
