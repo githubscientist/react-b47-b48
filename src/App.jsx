c9dd7800736d3dff70b9296cc45fc26284ffbac6
githubscientist <sathish.datascientist@gmail.com>
2023-09-23 17:22:08 +0530

    handling array list with foreach loop

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Note from './components/Note';
 
 function App(props) {
   const { notes } = props;
+  const itemList = [];
+
+  notes.forEach((note) => {
+    itemList.push(<Note key={note.id} note={note} />);
+  });
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {
-          notes.map((note) => {
-            return <Note key={ note.id } note={ note } />
-          })
-        }
+        { itemList }
       </ul>
     </div>
   )
