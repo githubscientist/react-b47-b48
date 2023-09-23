e63fdc30b01358d4db7b3545aa8a896a90467314
githubscientist <sathish.datascientist@gmail.com>
2023-09-23 16:38:13 +0530

    working with array lists - hardcoded the list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css';
+import React from 'react';
 
-const Hello = (props) => {
-  // console.log(props);
+function App(props) {
+  // console.log(props.notes);
+  const { notes } = props;
+
+  // console.log(notes);
+
   return (
     <div>
-      <p>Hello { props.name }, your age is { props.age }</p>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Sathish' age='30' />
-      <Hello name='Krish' age='10' />
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+        <li>{notes[3].content}</li>
+        <li>{notes[4].content}</li>
+      </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// data
+const notes = [
+    {
+        id: 1,
+        content: 'Working with Lists',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Usage of keys',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'Conditional Rendering',
+        important: true
+    },
+    {
+        id: 4,
+        content: 'Handling Events',
+        important: true
+    },
+    {
+        id: 5,
+        content: 'Props Vs State',
+        important: false
+    }
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={ notes } />);
