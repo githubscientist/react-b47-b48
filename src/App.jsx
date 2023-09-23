8e248efc7a3dac2d796f18ec46929f2016c647b4
githubscientist <sathish.datascientist@gmail.com>
2023-09-23 18:27:24 +0530

    event handling with form onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,25 @@ function App(props) {
 
   // console.log(optionList);
 
+  const addNote = (event) => {
+    event.preventDefault();
+    // console.log('button clicked');
+    // console.log(event.target);
+
+    // console.log(event.target.elements.note.value);
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         { optionList }
       </ul>
+
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>Save Note</button>
+      </form>
     </div>
   )
 }
