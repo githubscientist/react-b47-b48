48e6710d10be8b5efe3d4cf6810b5dd818dd5c44
githubscientist <sathish.datascientist@gmail.com>
2023-09-23 17:14:35 +0530

    moving Note to its own component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
-
-function Note({ note }) {
-
-  // const note = props.note;
-
-  return (
-    <li>{ note.content }</li>
-  )
-}
+import Note from './components/Note';
 
 function App(props) {
   const { notes } = props;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note({ note }) {
+
+  // const note = props.note;
+
+  return (
+    <li>{ note.content }</li>
+  )
+}
+
+export default Note;
