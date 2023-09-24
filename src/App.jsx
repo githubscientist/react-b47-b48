73a35af1929b0a37908f0596b609c04c898a9ff9
githubscientist <sathish.datascientist@gmail.com>
2023-09-24 16:38:32 +0530

    creating a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-// passing props in function component
-import React from 'react';
+import { Component } from "react";
 
-function Hello({ name }) {
-  return (
-    <div>Hello, { name }!</div>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <Hello name="Krish" />
-    </div>
-  )
+class App extends Component{
+  render() {
+    return <div>Hello Guest!</div>
+  }
 }
 
 export default App;
