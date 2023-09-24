3b8595e62fff09e335ba6feb2000ab3a329ab209
githubscientist <sathish.datascientist@gmail.com>
2023-09-24 17:52:16 +0530

    adding bootstrap using cdn in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-import { Component } from "react";
+import React from 'react';
+import './App.css';
 
-// stateful components
-class App extends Component {
-  constructor(props) {
-    super(props); // to call the parent class constructor
-
-    // define the state
-    this.state = {
-      count: 0,
-    }
-  }
-
-  handlePlusClick = () => {
-    // update the state (data) of the count
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }))
-  }
-
-  handleMinusClick = () => {
-    if (this.state.count > 0) {
-      // update the state (data) of the count
-      this.setState((prevState) => ({
-        count: prevState.count - 1,
-      }))
-    }
-  }
-
-  handleZeroClick = () => {
-    // update the state (data) of the count
-    this.setState((prevState) => ({
-      count: 0,
-    }))
-  }
-
-  render() {
-    return (
-      <div>
-        <p>Count: {this.state.count}</p>
-        <button onClick={this.handlePlusClick}>plus</button>
-        <button onClick={this.handleMinusClick}>minus</button>
-        <button onClick={this.handleZeroClick}>zero</button>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className='mt-4 p-2'>
+      <h1>Hello World!</h1>
+      <p className='redColor'>This paragraph will be in red color.</p>
+    </div>
+  )
 }
 
 export default App;
