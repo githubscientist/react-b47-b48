f6f07accd663a46d9621c125075751fc00f50695
githubscientist <sathish.datascientist@gmail.com>
2023-09-24 19:05:08 +0530

    exercise - stateful class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#!">Start Bootstrap</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <button className="btn btn-outline-dark" type="submit">
-                            <i className="bi-cart-fill me-1"></i>
-                            Cart
-                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-  )
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    }
+  }
+
+  handleGoodClick = () => {
+    this.setState({
+      ...this.state,
+      good: this.state.good + 1,
+    })
+  }
+
+  handleNeutralClick = () => {
+    this.setState({
+      ...this.state,
+      neutral: this.state.neutral + 1,
+    })
+  }
+
+  handleBadClick = () => {
+    this.setState({
+      ...this.state,
+      bad: this.state.bad + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={this.handleGoodClick}>good</button>
+        <button onClick={this.handleNeutralClick}>neutral</button>
+        <button onClick={this.handleBadClick}>bad</button>
+
+        <h2>statistics</h2>
+        <p>good { this.state.good }</p>
+        <p>neutral { this.state.neutral }</p>
+        <p>bad { this.state.bad }</p>
+      </div>
+    )
+  }
 }
-
-export default App;
