1977398fbc8c8b3901a1dc46e8110431f6938991
githubscientist <sathish.datascientist@gmail.com>
2023-10-08 16:42:13 +0530

    props drilling --parent to child and child to grandchild

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import GrandChildComponent from './GrandChildComponent';
 
-function ChildComponent({ message }) {
+function ChildComponent(props) {
+
+    const message = 'Hello, GrandChild!';
+
   return (
       <>
           <h2>Child Component</h2>
-          <p>Message from Parent Component: <b>{message}</b></p>
+          <p>Message from Parent Component: <b>{props.message}</b></p>
           <GrandChildComponent message={ message } />
       </>
   )
