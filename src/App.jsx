a58c695910a8ee6cad91da4b3f401e3aa5c290ce
githubscientist <sathish.datascientist@gmail.com>
2023-10-15 18:47:15 +0530

    redux state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state);
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        { counter }
+      </div>
+
+      <button onClick={e => dispatch({ type: 'INCR' })}>incr</button>
+      <button onClick={e => dispatch({ type: 'DECR' })}>decr</button>
+      <button onClick={e => dispatch({ type: 'ZERO' })}>zero</button>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import "bootstrap-icons/font/bootstrap-icons.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+let initialState = {
+    count: 0
+}
+
+const counterReducer = (state = initialState.count, action) => {
+    switch (action.type) {
+        case 'INCR':
+            return state + 1;
+        case 'DECR':
+            return state - 1;
+        case 'ZERO':
+            return 0;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
