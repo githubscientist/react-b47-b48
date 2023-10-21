3a2b26b17f0989b8bbb2d0d363cc688e5ec84482
githubscientist <sathish.datascientist@gmail.com>
2023-10-21 16:43:24 +0530

    view notes with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import { createStore } from 'redux';
+
+const noteReducer = (state = [], action) => {
+  if (action.type == 'NEW_NOTE') {
+    state.push(action.payload);
+    return state;
+  }
+
+  return state;
+}
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: 'NEW_NOTE', 
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE', 
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id:2,
+  }
+})
 
 function App() {
+  // console.log(store.getState());
   return (
-    <div>App</div>
+    <div>
+      <ul>
+        {
+          store.getState().map(note => 
+            <li key={note.id}>
+              {note.content} <strong>{ note.important ?  '★' : ''}</strong>
+            </li>
+          )
+        }
+      </ul>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import "bootstrap-icons/font/bootstrap-icons.css";
-
-// define the reducer function
-// initial state and action
-// return : new State
-const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCR':
-            return state + 1;
-        case 'DECR':
-            return state - 1;
-        case 'ZERO':
-            return 0;
-        default:
-            return state;
-    }
-}
-
-// create a new store
-const store = createStore(counterReducer);
-
-// subscribe method is called whenever we dispatch an action
-store.subscribe(() => {
-    const storeNow = store.getState();
-    console.log(storeNow);
-})
-
-store.dispatch({ type: 'INCR' });
-store.dispatch({ type: 'INCR' });
-store.dispatch({ type: 'DECR' });
-store.dispatch({ type: 'INCR' });
-store.dispatch({ type: 'INCR' });
-store.dispatch({ type: 'ZERO' });
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
