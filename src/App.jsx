4813644b5898a59ee131ac4ed157b6463d606dcd
githubscientist <sathish.datascientist@gmail.com>
2023-10-21 17:00:09 +0530

    redux with notes app

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ import { createStore } from 'redux';
 
 const noteReducer = (state = [], action) => {
   if (action.type == 'NEW_NOTE') {
-    state.push(action.payload);
-    return state;
+    
+  }
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.payload);
+    case 'TOGGLE_IMPORTANCE':
+      // get the id from the action.payload
+      const id = action.payload.id;
+      const noteToChange = state.find(n => n.id === id);
+    // console.log(noteToChange);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      }
+
+      return state.map(note => 
+        note.id !== id ? note: changedNote
+      )
   }
 
   return state;
@@ -27,6 +43,20 @@ store.dispatch({
     content: 'state changes are made with actions',
     important: false,
     id:2,
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  payload: {
+    id: 2,
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  payload: {
+    id: 1,
   }
 })
 
